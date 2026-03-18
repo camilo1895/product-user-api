fe65c479a0276aed48d923ff8019b42d9030d724
camilo1895 <camiloguarnizo1895@gmail.com>
2026-03-18 15:08:21 -0500

    Inicio de proyecto

---
+++ src/main/java/cg/product_user_api/ProductUserApiApplication.java
@@ -0,0 +1,13 @@
+package cg.product_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cg/product_user_api/ProductUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.product_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
